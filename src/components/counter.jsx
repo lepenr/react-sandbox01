a480f7d5275bb68917844324fe3bd6dd51bbea63
lepenr <lepenr@icloud.com>
2018-09-09 19:24:39 +0200

    first commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+class Counter extends Component {
+  state = {
+    count: 0
+  };
+  render() {
+    //let classes = this.getBadgeClasses();
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 btn-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
